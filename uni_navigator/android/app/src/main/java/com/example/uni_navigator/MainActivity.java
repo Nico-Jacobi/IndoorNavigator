21f9ae571d5cd9726721921c6a85e3782aef47cd
nico <nico@retriever-web.de>
2025-03-28 22:36:00 +0100

    added flutter project with webviewer

---
+++ uni_navigator/android/app/src/main/java/com/example/uni_navigator/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.uni_navigator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ uni_navigator/android/app/src/main/kotlin/com/example/uni_navigator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uni_navigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
